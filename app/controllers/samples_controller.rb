919bfa4ec94f43b2d5d5b80ccec8462a356898ce
Storycoder <joseph.margolis@gmail.com>
2015-12-04 21:35:44 -0500

    added index exception to auth req & conditional statements index based on being signed in

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -1,6 +1,7 @@
 class SamplesController < ApplicationController
-	before_action :authenticate_user!
+	before_action :authenticate_user!, except: [:index]
 	
+
 	def index
 		@samples = Sample.all
 	end
